3d0be976853e28c5a6b3ad9227f00a1e22c4f424
koushiksoft <koushiksoft@gmail.com>
2013-03-08 00:18:56 +0530

    Navigate Between Activity - Created

---
+++ src/com/hell2pal/navigatebetweenactivity/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.hell2pal.navigatebetweenactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.another_activity);
+	}
+}
---
+++ src/com/hell2pal/navigatebetweenactivity/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hell2pal.navigatebetweenactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	private Button button; //
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*
+		 * Each activity must have a corresponding layout XML file
+		 */
+		setContentView(R.layout.activity_main);// activity_main is corresponding
+												// XML file
+
+		/*
+		 * Initializing button XML button id. findViewById is a method which
+		 * helps to initialize with particular id. btn_go_to_another_activity is
+		 * a button name which I have given in XML file
+		 */
+		button = (Button) findViewById(R.id.btn_go_to_another_activity);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				/*
+				 * Intent is just like glue which helps to navigate one activity
+				 * to another.
+				 */Intent intent = new Intent(MainActivity.this,
+						AnotherActivity.class);
+				startActivity(intent); // startActivity allow you to move
+			}
+		});
+	}
+
+}
